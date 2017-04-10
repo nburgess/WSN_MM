1590f2506f714dff441bbf4d496bc4b837a0a336
Trent Walls <Trent@SwitchitDesign.com>
2017-04-09 20:43:46 -0400

    added icons to all

--- WSNTRENT/WSN_MM/MICROMOUSE/combo/coreStartup.py
+++ WSNTRENT/WSN_MM/MICROMOUSE/combo/coreStartup.py
@@ -33,13 +33,15 @@ class coreStartupObj(threading.Thread):
                 sleep(self.COMMAND_DELAY)
                 print("Starting node 2...")
                 call("coresendmsg exec node=2 num=1001 cmd='python /home/core/code/WSNTRENT/WSN_MM/MICROMOUSE/combo/mouse.py 1'", shell=True)
+                call("coresendmsg node number=2 icon=/home/core/code/coreConfig/car.png", shell=True)
                 sleep(self.COMMAND_DELAY)
                 print("Starting node 3...")
                 call("coresendmsg exec node=3 num=1001 cmd='python /home/core/code/WSNTRENT/WSN_MM/MICROMOUSE/combo/mouse.py 1'", shell=True)
+                call("coresendmsg node number=3 icon=/home/core/code/coreConfig/car.png", shell=True)
                 sleep(self.COMMAND_DELAY)
                 print("Starting node 4...")
                 call("coresendmsg exec node=4 num=1001 cmd='python /home/core/code/WSNTRENT/WSN_MM/MICROMOUSE/combo/mouse.py 1'", shell=True)
-            
+                call("coresendmsg node number=4 icon=/home/core/code/coreConfig/car.png", shell=True)
 
 
 
