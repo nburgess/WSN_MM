d3f951744fa74dfe76626c579488556e6cf99ef9
Trent Walls <Trent@SwitchitDesign.com>
2017-04-10 03:06:41 -0400

    Finished map visualization on website

--- website/uploadMap.php
+++ website/uploadMap.php
@@ -1,5 +1,53 @@
 <?php
-	
-	
+	echo "v3";
+	$con = mysqli_connect("localhost","MM","mm","MM");
 
+	$MAP_SIZE=1089;//33 x 33
+
+	$mouse=intval($_POST['mouse']);
+	$options_raw=$_POST['options'];
+	$types_raw=$_POST['types'];
+	
+	
+	if ($mouse!=1 && $mouse!=2 && $mouse!=3 && $mouse!=4)
+	{
+		echo "INVALID MOUSE ID!";
+		mysqli_query($con,"INSERT INTO log (msg) VALUES ('M_ID');") or die("ERROR");
+		exit;
+	}
+	
+	$options=json_decode($options_raw);
+	$types=json_decode($types_raw);
+	
+	if (sizeof($options)!=$MAP_SIZE || sizeof($types)!=$MAP_SIZE)
+	{
+		echo "MAP WRONG SIZE";
+		mysqli_query($con,"INSERT INTO log (msg) VALUES ('SIZE');") or die("ERROR");
+		exit;
+	}
+	
+	//echo $options[0].",";
+	//echo $options[1].",";
+	//echo $options[2].",";
+	//echo $options[3].",";
+	
+	//echo $types[0].",";
+	//echo $types[1].",";
+	//echo $types[2].",";
+	//echo $types[3].",";
+	
+	$count=0;
+	for ($y=0;$y<=32;$y++)
+	{
+		for ($x=0;$x<=32;$x++)
+		{	
+			//echo $x.','.$y.'\n';
+			mysqli_query($con,"UPDATE mouse_maps SET option_col=".intval($options[$count]).",type_col=".intval($types[$count])." WHERE mouse_id=".$mouse." AND x_pos=".$x." AND y_pos=".$y.";") or die("ERROR");
+			$count++;
+		}
+	}
+
+	mysqli_query($con,"INSERT INTO log (msg) VALUES ('SUCCESS');") or die("ERROR");
+	echo "SUCCESS";
+	
 ?>
---
+++ website/wsnvisual.php
@@ -0,0 +1,200 @@
+<?php
+	$con = mysqli_connect("localhost","MM","mm","MM");
+	
+	$map_datas1=mysqli_query($con,"SELECT option_col,type_col,x_pos,y_pos FROM mouse_maps WHERE mouse_id=1 ORDER BY y_pos ASC,x_pos ASC") or die("ERROR");
+	$map_datas2=mysqli_query($con,"SELECT option_col,type_col,x_pos,y_pos FROM mouse_maps WHERE mouse_id=2 ORDER BY y_pos ASC,x_pos ASC") or die("ERROR");
+	$map_datas3=mysqli_query($con,"SELECT option_col,type_col,x_pos,y_pos FROM mouse_maps WHERE mouse_id=3 ORDER BY y_pos ASC,x_pos ASC") or die("ERROR");
+	$map_datas4=mysqli_query($con,"SELECT option_col,type_col,x_pos,y_pos FROM mouse_maps WHERE mouse_id=4 ORDER BY y_pos ASC,x_pos ASC") or die("ERROR");
+?>
+
+<html>
+    <head>
+        <title>Virtual Micromouse Project</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body style="background-color:#FFACD;font: normal 100% 'century gothic', arial, sans-serif;">
+
+
+        <div>
+          <div id="banner">
+            <div id="banner-content" align="center">
+              <img src="logo2.png" alt="Mountain View" style="width:600px;height:69px;">
+            </div>
+          </div>
+          <ul id="nav" align="center">
+                    <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+                    <li class="selected"><a style="color: white;text-decoration: none;" href="index.html">Home | </a></li>
+                    <li><a style="color: white;text-decoration: none;" href="examples.html">Examples | </a></li>
+                    <li><a style="color: white;text-decoration: none;" href="contact.html">Contact Us</a></li>
+          </ul>
+          <h1 align="center">Virtual Micromouse Project</h1>
+          <h3 align="center">Trent Walls | Nicolas Burgess | Vivek Patel | Brian Lee</h3>
+			<h1>Mouse 1</h1>
+			<?php
+			for ($y=0;$y<=32;$y++)
+			{
+				echo '<div class="Row">';
+				for ($x=0;$x<=32;$x++)
+				{	
+					$map_data=mysqli_fetch_array($map_datas1);
+					//echo "(".$map_data[2].",".$map_data[3].")<br>";
+					$option=intval($map_data[0]);
+					$type=intval($map_data[1]);
+					
+					if ($type==8)
+					{
+						echo '<div id="cell" style="background-color:black;"></div>';
+					}
+					else if ($type==1)
+					{
+						echo '<div id="cell" style="background-color:#581845;"></div>';
+					}
+					else
+					{
+						if ($option==0)
+						{
+							echo '<div id="cell" style="background-color:#900C3F;"></div>';
+						}
+						else if($option==1)
+						{
+							echo '<div id="cell" style="background-color:#FF5733;"></div>';
+						}
+						else
+						{						
+							echo '<div id="cell" style="background-color:#C70039;"></div>';
+						}
+						
+					}
+				}
+				echo '</div>';
+			}
+			?>
+			<br><br>
+    		<h1>Mouse 2</h1>
+			<?php
+			for ($y=0;$y<=32;$y++)
+			{
+				echo '<div class="Row">';
+				for ($x=0;$x<=32;$x++)
+				{	
+					$map_data=mysqli_fetch_array($map_datas2);
+					//echo "(".$map_data[2].",".$map_data[3].")<br>";
+					$option=intval($map_data[0]);
+					$type=intval($map_data[1]);
+					
+					if ($type==8)
+					{
+						echo '<div id="cell" style="background-color:black;"></div>';
+					}
+					else if ($type==1)
+					{
+						echo '<div id="cell" style="background-color:#581845;"></div>';
+					}
+					else
+					{
+						if ($option==0)
+						{
+							echo '<div id="cell" style="background-color:#900C3F;"></div>';
+						}
+						else if($option==1)
+						{
+							echo '<div id="cell" style="background-color:#FF5733;"></div>';
+						}
+						else
+						{						
+							echo '<div id="cell" style="background-color:#C70039;"></div>';
+						}
+						
+					}
+				}
+				echo '</div>';
+			}
+			?>
+			<br><br>
+			<h1>Mouse 3</h1>
+			<?php
+			for ($y=0;$y<=32;$y++)
+			{
+				echo '<div class="Row">';
+				for ($x=0;$x<=32;$x++)
+				{	
+					$map_data=mysqli_fetch_array($map_datas3);
+					//echo "(".$map_data[2].",".$map_data[3].")<br>";
+					$option=intval($map_data[0]);
+					$type=intval($map_data[1]);
+					
+					if ($type==8)
+					{
+						echo '<div id="cell" style="background-color:black;"></div>';
+					}
+					else if ($type==1)
+					{
+						echo '<div id="cell" style="background-color:#581845;"></div>';
+					}
+					else
+					{
+						if ($option==0)
+						{
+							echo '<div id="cell" style="background-color:#900C3F;"></div>';
+						}
+						else if($option==1)
+						{
+							echo '<div id="cell" style="background-color:#FF5733;"></div>';
+						}
+						else
+						{						
+							echo '<div id="cell" style="background-color:#C70039;"></div>';
+						}
+						
+					}
+				}
+				echo '</div>';
+			}
+			?>
+			<br><br>
+			<h1>Mouse 4</h1>
+			<?php
+			for ($y=0;$y<=32;$y++)
+			{
+				echo '<div class="Row">';
+				for ($x=0;$x<=32;$x++)
+				{	
+					$map_data=mysqli_fetch_array($map_datas4);
+					//echo "(".$map_data[2].",".$map_data[3].")<br>";
+					$option=intval($map_data[0]);
+					$type=intval($map_data[1]);
+					
+					if ($type==8)
+					{
+						echo '<div id="cell" style="background-color:black;"></div>';
+					}
+					else if ($type==1)
+					{
+						echo '<div id="cell" style="background-color:#581845;"></div>';
+					}
+					else
+					{
+						if ($option==0)
+						{
+							echo '<div id="cell" style="background-color:#900C3F;"></div>';
+						}
+						else if($option==1)
+						{
+							echo '<div id="cell" style="background-color:#FF5733;"></div>';
+						}
+						else
+						{						
+							echo '<div id="cell" style="background-color:#C70039;"></div>';
+						}
+						
+					}
+				}
+				echo '</div>';
+			}
+			?>
+
+            <div class="Row">
+          </div>
+        </div>
+    </body>
+</html>
